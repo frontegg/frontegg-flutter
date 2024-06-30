44aad17a12c72258988b3e31e9554fee9dfb6d1c
David Antoon <david@frontegg.com>
2024-06-30 15:10:02 +0300

    Update native sdks

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -19,7 +19,7 @@ import FronteggSwift
      */
     override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         
-        if(FronteggAuth.shared.handleOpenUrl(url)){
+        if(FronteggAuth.shared.handleOpenUrl(url, true)){
             return true
         }
         
@@ -34,7 +34,7 @@ import FronteggSwift
     override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         
         if let url = userActivity.webpageURL {
-            if(FronteggAuth.shared.handleOpenUrl(url)){
+            if(FronteggAuth.shared.handleOpenUrl(url, true)){
                 return true
             }
         }
