a2724265dd6ab43ea62746733e5519b5cdc215cc
alex-minaiev-frontegg <alex.minaiev@frontegg.com>
2024-12-12 16:56:01 +0200

    FR-18979: Updated run script

--- run_integration_test.sh
+++ run_integration_test.sh
@@ -1,17 +1,50 @@
 #!/bin/sh
 
-cd example
+while getopts ":a:i:t:" opt; do
+  case $opt in
+    a) android_device="$OPTARG"
+    ;;
+    i) ios_device="$OPTARG"
+    ;;
+    t) test_path="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+cd example || exit 1
 
 flutter pub global activate patrol_cli
 
 patrol doctor
 
-# iOS run
-patrol test -d 'iPhone 16 Pro Max' -t integration_test/src/switch_tenant_test.dart --uninstall
 
+if [ -z "$test_path" ]; then
+  test_path="integration_test/src/"
+fi
+
+echo "Starting test on $test_path"
+
+# iOS run
+if [ -n "$ios_device" ]; then
+  echo "Start iOS testing on '$ios_device' device..."
+  patrol test -d "$ios_device" -t "$test_path" --uninstall
+fi
 
 # Android run
-patrol test -d emulator-5554 -t integration_test/src/sign_up_via_email_and_password_test.dart
+if [ -n "$android_device" ]; then
+  echo "Start iOS testing on '$android_device' device..."
+  patrol test -d "$android_device" -t "$test_path" sign_up_via_email_and_password_test.dart
+fi
+
 
 
 
