16aa9e65225319d613a1173e805e9969cf7cc688
alex-minaiev-frontegg <alex.minaiev@frontegg.com>
2025-03-24 18:35:52 +0200

    FR-19654: Added CustomLoader functioning

--- example/android/app/src/main/kotlin/com/frontegg/demo/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/frontegg/demo/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.frontegg.demo
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.ProgressBar
+import com.frontegg.android.ui.DefaultLoader
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Setup Loader for Frontegg Embedded Activity Loading
+        DefaultLoader.setLoaderProvider {
+            val progressBar = ProgressBar(it)
+            val colorStateList = ColorStateList.valueOf(Color.GREEN)
+            progressBar.indeterminateTintList = colorStateList
+
+            progressBar
+        }
+    }
+}
