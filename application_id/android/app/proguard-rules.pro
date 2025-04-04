250276e1a89aa64da046fef37089fc0b36c271fa
alex-minaiev-frontegg <alex.minaiev@frontegg.com>
2025-04-04 14:42:30 +0300

    FR-19895: Added applicationId example project

---
+++ application_id/android/app/proguard-rules.pro
@@ -0,0 +1,23 @@
+#Flutter Wrapper
+-keep class io.flutter.app.** { *; }
+-keep class io.flutter.plugin.**  { *; }
+-keep class io.flutter.util.**  { *; }
+-keep class io.flutter.view.**  { *; }
+-keep class io.flutter.**  { *; }
+-keep class io.flutter.plugins.**  { *; }
+
+# Frontegg SDK
+
+-keepclasseswithmembers class com.frontegg.** {*;}
+
+-dontwarn com.google.android.play.core.splitcompat.SplitCompatApplication
+-dontwarn com.google.android.play.core.splitinstall.SplitInstallException
+-dontwarn com.google.android.play.core.splitinstall.SplitInstallManager
+-dontwarn com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+-dontwarn com.google.android.play.core.splitinstall.SplitInstallRequest$Builder
+-dontwarn com.google.android.play.core.splitinstall.SplitInstallRequest
+-dontwarn com.google.android.play.core.splitinstall.SplitInstallSessionState
+-dontwarn com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
+-dontwarn com.google.android.play.core.tasks.OnFailureListener
+-dontwarn com.google.android.play.core.tasks.OnSuccessListener
+-dontwarn com.google.android.play.core.tasks.Task
---
+++ application_id/lib/tenants_tab.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:frontegg_flutter/frontegg_flutter.dart';
+
+/// TenantsTab
+/// 
+/// This is a stateless widget that builds the tenants tab.
+/// It is used to switch between tenants.
+/// 
+class TenantsTab extends StatelessWidget {
+  const TenantsTab({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final frontegg = context.frontegg;
+
+    // StreamBuilder to listen to the state of the authentication
+    return StreamBuilder<FronteggState>(
+      stream: frontegg.stateChanged,
+      builder: (BuildContext context, AsyncSnapshot<FronteggState> snapshot) {
+        if (snapshot.hasData && snapshot.data?.user != null) {
+          final state = snapshot.data!;
+          final user = state.user!;
+          if (state.isLoading) {
+            // If the app is loading, show a loading indicator
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+           // If the user is authenticated and the user is not null, show the tenants tab
+          return SingleChildScrollView(
+            child: Column(
+              children: user.tenants.map(
+                (e) {
+                  final isActive = user.activeTenant.tenantId == e.tenantId;
+                  return ListTile(
+                    key: ValueKey(e.tenantId),
+                    title: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: e.name,
+                            style: const TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                          if (isActive)
+                            const TextSpan(
+                              text: " (active)",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.black,
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      // Switch to the tenant by providing the tenant id
+                      frontegg.switchTenant(e.tenantId);
+                    },
+                  );
+                },
+              ).toList(),
+            ),
+          );
+        }
+
+        return const SizedBox();
+      },
+    );
+  }
+}
