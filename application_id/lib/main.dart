94048234b4a911a526f9c763a802ae99d0ad3338
alex-minaiev-frontegg <alex.minaiev@frontegg.com>
2025-04-04 14:42:30 +0300

    FR-19895: Added applicationId example project

---
+++ application_id/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:frontegg_flutter/frontegg_flutter.dart';
+import 'package:frontegg_flutter_application_id_example/main_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+/// MyApp
+/// 
+/// This is the main app that is used to run the app.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Wrap the MainPage with the FronteggProvider to Frontegg SDK access
+      home: FronteggProvider(
+        child: const MainPage(),
+      ),
+    );
+  }
+}
---
+++ application_id/lib/main_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:frontegg_flutter/frontegg_flutter.dart';
+import 'package:frontegg_flutter_application_id_example/login_page.dart';
+import 'package:frontegg_flutter_application_id_example/user_page.dart';
+
+/// Main page
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final frontegg = context.frontegg;
+    return Scaffold(
+      body: Center(
+        // StreamBuilder to listen to the state of the authentication
+        child: StreamBuilder<FronteggState>(
+          stream: frontegg.stateChanged,
+          builder:
+              (BuildContext context, AsyncSnapshot<FronteggState> snapshot) {
+            if (snapshot.hasData) {
+              final state = snapshot.data!;
+              if (state.isAuthenticated && state.user != null) {
+                // If the user is authenticated and the user is not null, show the user page
+                return const UserPage();
+              } else if (state.initializing) {
+                // If the app is initializing, show a loading indicator
+                return const CircularProgressIndicator();
+              } else {
+                // If the user is not authenticated, show the login page
+                return const LoginPage();
+              }
+            }
+
+            return const SizedBox();
+          },
+        ),
+      ),
+    );
+  }
+}
